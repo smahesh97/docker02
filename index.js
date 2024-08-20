a4f1b129cd552719658960fde49f3ccc5661ac63
smahesh97 <smahesh.97@gmail.com>
2024-08-20 20:08:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi Mahesh, I know not with what weapons World War III will be fought, but World War IV will be fought with sticks and stones.”
-― Albert Einstein:");
+  res.send("Hi Mahesh, I know not with what weapons World War III will be fought, but World War IV will be fought with sticks and stones. — Albert Einstein");
 });
+
 
 app.get("/info", (req, res) => {
   res.send("hey this is /info api");
