685dd314698ff3512df3aba2e194793b26ee0942
smahesh97 <smahesh.97@gmail.com>
2024-08-20 18:19:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get("/info", (req, res) => {
   res.send("hey this is /info api");
 });
 
-app.listen(80, () => {
-  console.log("listening on 3000");
+app.listen(81, () => {
+  console.log("listening on 81");
 });
