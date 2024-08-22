6ea52a6ff6a9f1f5165560e9690d03bec38fd860
smahesh97 <smahesh.97@gmail.com>
2024-08-22 15:12:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi Mahesh Subramaniyan, I know not with what weapons World War III will be fought, but World War IV will be fought with sticks and stones. — Albert Einstein");
+  res.send("Hi Kumaresan Venkatesan, I know not with what weapons World War III will be fought, but World War IV will be fought with sticks and stones. — Albert Einstein");
 });
 
 
